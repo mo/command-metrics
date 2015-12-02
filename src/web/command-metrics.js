b9bcbcbf5e9f857e5114d6b7c976ecec7cdc6a31
Martin Olsson <martin@minimum.se>
2015-12-02 23:02:49 +0100

    minimal first version

---
+++ src/web/command-metrics.js
@@ -0,0 +1,86 @@
+function populateSelectBox(allChartData, selectedChart) {
+    for (var i = 0; i < allChartData.length; i++) {
+        chart_data = allChartData[i];
+        var option = document.createElement("option");
+        option.innerHTML = chart_data["chartTitle"];
+        option.value = chart_data["chartKey"];
+        document.getElementById("chart-select").appendChild(option);
+    }
+    document.getElementById('chart-select').value = selectedChart["chartKey"]
+    document.getElementById('chart-select').focus()
+}
+
+function getSeriesXDataNames(chartData) {
+    var xDataNames = {};
+
+    var allSeries = chartData["chartSeries"];
+    for (var i = 0; i < allSeries.length; i++) {
+        xDataNames["data" + i ] = "x" + i;
+    }
+
+    return xDataNames;
+}
+
+function getSeriesData(chartData) {
+    var seriesData = [];
+
+    var allSeries = chartData["chartSeries"];
+    for (var i = 0; i < allSeries.length; i++) {
+        var xData = allSeries[i]["xValues"];
+        xData.unshift("x" + i);
+        seriesData.push(xData);
+
+        var yData = allSeries[i]["yValues"];
+        yData.unshift("data" + i);
+        seriesData.push(yData);
+    }
+
+    return seriesData;
+}
+
+function getSeriesColors(chartData) {
+    var colors = {};
+    var allSeries = chartData["chartSeries"];
+    for (var i = 0; i < allSeries.length; i++) {
+        colors["data" + i] = allSeries[i]["seriesColor"];
+    }
+    return colors;
+}
+
+function getSeriesTitles(chartData) {
+    var seriesTitles = {};
+    var allSeries = chartData["chartSeries"];
+    for (var i = 0; i < allSeries.length; i++) {
+        seriesTitles["data" + i] = allSeries[i]["seriesTitle"];
+    }
+    return seriesTitles;
+}
+
+function renderChart(chartData) {
+    var seriesXDataNames = getSeriesXDataNames(chartData);
+    var seriesColumns = getSeriesData(chartData);
+    var seriesColors = getSeriesColors(chartData);
+    var seriesTitles = getSeriesTitles(chartData);
+
+    var chart = c3.generate({
+        padding: {
+            left: 25,
+            right: 25,
+        },
+        data: {
+            xFormat: '%Y-%m-%d %H:%M:%S',
+            xs: seriesXDataNames,
+            columns: seriesColumns,
+            colors: seriesColors,
+            names: seriesTitles,
+        },
+        axis: {
+            x: {
+                type: 'timeseries',
+                tick: {
+                    format: '%Y-%m-%d'
+                }
+            }
+        }
+    });
+}
