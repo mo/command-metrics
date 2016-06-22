19cf97bc365ca864a35a98a9935ebdc126c00b7d
Martin Olsson <martin@minimum.se>
2016-06-22 23:57:28 +0200

    Add script to be called without parameters from cron

---
+++ cron.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#
+# NOTE: Open "crontab -e" and insert call to this script (with no parameters).
+# And also put:
+# MAILTO=""
+# ...at the top of the crontab file. Look for errors in:
+# /var/log/command-metrics-cron.log
+#
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+$SCRIPT_DIR/src/refresh-measurements -s 3 >> /var/log/command-metrics-cron.log 2>&1
+
