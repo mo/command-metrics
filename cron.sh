ab920f5a133472fbc43a20fc7c2fd33d56204fb7
Martin Olsson <martin@minimum.se>
2016-10-13 09:33:06 +0200

    Change default log location

--- cron.sh
+++ cron.sh
@@ -2,12 +2,14 @@
 
 #
 # NOTE: Open "crontab -e" and insert call to this script (with no parameters).
+#
 # And also put:
 # MAILTO=""
-# ...at the top of the crontab file. Look for errors in:
-# /var/log/command-metrics-cron.log
+# ...at the top of the crontab file.
+#
+# Look for errors in "command-metrics-$(hostname).log"
 #
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-$SCRIPT_DIR/src/refresh-measurements measure >> /var/log/command-metrics-cron.log 2>&1
+$SCRIPT_DIR/src/refresh-measurements measure >> $SCRIPT_DIR/../command-metrics-$(hostname).log 2>&1
 
